ae1962035fa8ce2a72bb027ad3206e935d95957a
Simic Stefan <simicstefan@hakzell.schueler.at>
2026-03-16 14:19:34 +0100

    adding code

---
+++ Uebungen20260316.Wiederholung/Haus.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace Uebungen20260316.Wiederholung;
+
+public class Haus
+{
+    private double _qm;
+    public Haus(double qm, string _farbe)
+    {
+        Quadratmeter = qm;
+        Farbe = _farbe;
+
+    }
+    public string Farbe { get; private set; } = "weiß"; 
+    public double Quadratmeter
+    {
+        get { return _qm; }
+        set
+        {
+            if (value < 50)
+            {
+                Console.WriteLine("Quadratmeter darf nicht weniger als 50 sein.");
+            }
+            else
+            {
+                _qm = value;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ Uebungen20260316.Wiederholung/Program.cs
@@ -0,0 +1,5 @@
+﻿using Uebungen20260316.Wiederholung;
+
+Haus h1 = new Haus(350);
+
+Console.WriteLine($"Das Haus hat {h1.Quadratmeter} m2.");
