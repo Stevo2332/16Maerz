82953ab7b369f68197a398b4eefd1d84f68b5016
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 20:01:58 +0100

    Fragenbeantwortung

---
+++ UebungFragen/UebungFragen.20260316/Program.cs
@@ -0,0 +1,14 @@
+﻿// Frage 1: Unterschied zwischen Klasse und Objekt
+// Antwort: Eine Klasse ist wie ein Bauplan oder eine Vorlage,
+// die beschreibt, wie ein Objekt aussehen und sich verhalten soll.
+// Ein Objekt hingegen ist eine konkrete Instanz einer Klasse, die im Speicher existiert und die Eigenschaften und Methoden der Klasse nutzen kann.
+
+// Frage 2: Warum verwendet man Namespaces in C#?
+// Antwort: Namespaces werden verwendet, um den Code zu organisieren und Namenskonflikte zu vermeiden.
+// Sie ermöglichen es, Klassen, Schnittstellen und andere Typen in logische Gruppen zu unterteilen, was die Wartbarkeit und Lesbarkeit des Codes verbessert.
+
+
+
+// Frage 3: Was ist der Unterschied zwischen einer Eigenschaft und einer Variable in C#?
+// Antwort: Eine Varibale ist ein Speicherort, der einen Wert speichern kann,
+// während eine Eigenschaft eine spezielle Art von Mitglied ist, die den Zugriff auf einen Wert ermöglicht. 
