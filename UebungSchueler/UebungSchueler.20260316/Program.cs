8a026fd8883ff77892749ede661ca7512b6a0505
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 19:17:02 +0100

    adding code

---
+++ UebungSchueler/UebungSchueler.20260316/Program.cs
@@ -0,0 +1,10 @@
+﻿using UebungSchueler._20260316;
+
+Student student1 = new Student();
+
+student1.FirstName = "Max";
+student1.LastName = "Mustermann";
+student1.Mainclass = "2BHK";
+
+
+student1.SayHello();
---
+++ UebungSchueler/UebungSchueler.20260316/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace UebungSchueler._20260316;
+
+public class Student
+{
+public string FirstName { get; set; }
+
+public string LastName { get; set; }    
+
+public string Mainclass { get; set; }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hallo ich bin {FirstName} {LastName} und bin Schueler der {Mainclass} ");
+    }
+
+}
