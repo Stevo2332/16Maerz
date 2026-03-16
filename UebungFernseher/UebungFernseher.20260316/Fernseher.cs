f231a7baa19c8548bbaea7cb081d76558f4e58ca
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 20:54:50 +0100

    adding task Fernseher

---
+++ UebungFernseher/UebungFernseher.20260316/Fernseher.cs
@@ -0,0 +1,30 @@
+﻿namespace UebungFernseher._20260316;
+
+public class Fernseher
+{
+    public string Marke { get; set; }
+    public int Kanal { get; set; }
+    public bool IstAn { get; set; }
+
+    public void Markenzeichen()
+    {
+        Console.WriteLine($"Die Marke des Fernsehers ist: {Marke}");
+    }
+
+    public void Einschalten()
+    {
+        IstAn = true;
+        Console.WriteLine("Der Fernseher ist eingeschaltet.");
+    }
+
+    public void Ausschalten()
+    {
+        IstAn = false;
+        Console.WriteLine("Der Fernseher ist ausgeschaltet.");
+    }
+
+    public void KanalAnzeigen()
+    {
+        Console.WriteLine($"Aktueller Kanal: {Kanal}");
+    }
+}
---
+++ UebungFernseher/UebungFernseher.20260316/Program.cs
@@ -0,0 +1,12 @@
+﻿using UebungFernseher._20260316;
+
+Fernseher fernseher = new Fernseher();
+fernseher.Marke = "Samsung";
+fernseher.IstAn = true;
+fernseher.Kanal = 5;
+fernseher.IstAn = false;
+
+fernseher.Markenzeichen();
+fernseher.Einschalten();
+fernseher.KanalAnzeigen();
+fernseher.Ausschalten();    
