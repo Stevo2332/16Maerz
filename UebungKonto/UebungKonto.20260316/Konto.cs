4263efdf95c722a45427b2fbf0669a4a8558e4a5
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 20:30:18 +0100

    Kontotask

---
+++ UebungKonto/UebungKonto.20260316/Konto.cs
@@ -0,0 +1,23 @@
+﻿namespace UebungKonto._20260316
+{
+    public class Konto
+    {
+    public string Inhaber { get; set; } 
+    public int Kontostand { get; set; }  
+
+    
+        public void KontostandAnzeigen()
+        {
+            Console.WriteLine($"Der Kontostand von {Inhaber} beträgt: {Kontostand}");
+        }
+
+        public void Einzahlen()
+        {
+
+            Kontostand = Kontostand + 100; // Hier wird der Kontostand um 100 erhöht, da die Einzahlung 100 Euro beträgt    
+            Console.WriteLine("Einzahlung erfolgreich.");
+            
+        
+        }
+    }
+}
---
+++ UebungKonto/UebungKonto.20260316/Program.cs
@@ -0,0 +1,9 @@
+﻿using UebungKonto._20260316;
+
+Konto konto1 = new Konto(); 
+konto1.Inhaber = "Max Mustermann";  
+konto1.Kontostand = 1000;
+konto1.KontostandAnzeigen();    
+konto1.Einzahlen();
+konto1.KontostandAnzeigen();
+
