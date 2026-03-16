f930e61772c3fc90105fc0c141d3d69398f382c0
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 19:45:03 +0100

    adding task Hund

---
+++ UebungHund/UebungHund.20260316/Hund.cs
@@ -0,0 +1,25 @@
+﻿namespace UebungHund._20260316;
+
+public class Hund
+{
+    public string Name { get; set; }
+    public string Rasse { get; set; }
+
+    public int Alter { get; set; }
+
+    public void Bellen()
+    {
+        Console.WriteLine($"Der Hund {Name} bellt");
+    }
+
+    public void Info() {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Rasse: {Rasse}");
+        Console.WriteLine($"Alter: {Alter}");
+    
+    }  
+
+
+
+}
+
---
+++ UebungHund/UebungHund.20260316/Program.cs
@@ -0,0 +1,11 @@
+﻿using UebungHund._20260316;
+
+Hund hund1 = new Hund();
+
+hund1.Name = "Bello";
+hund1.Rasse = "Labrador";
+hund1.Alter = 4;
+
+
+hund1.Bellen();
+hund1.Info();   
