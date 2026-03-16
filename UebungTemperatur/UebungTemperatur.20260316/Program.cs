f378854506423d56c0259ee3063e57bc67c79958
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 20:36:36 +0100

    Temperaturtask

---
+++ UebungTemperatur/UebungTemperatur.20260316/Program.cs
@@ -0,0 +1,11 @@
+﻿using UebungTemperatur._20260316;
+
+
+Temperatur temp1 = new Temperatur();
+
+temp1.GradCelsius = 25.5;
+temp1.Anzeigen();
+temp1.Erhoehen();
+temp1.Erhoehen();
+temp1.Senken();
+temp1.Anzeigen();
---
+++ UebungTemperatur/UebungTemperatur.20260316/Temperatur.cs
@@ -0,0 +1,25 @@
+﻿namespace UebungTemperatur._20260316;
+
+public class Temperatur
+{
+    public double GradCelsius { get; set; }
+
+    public void Anzeigen()
+    {
+        Console.WriteLine($"Die Temperatur beträgt {GradCelsius} °C");
+
+    }
+    public void Erhoehen()
+    {
+        GradCelsius += 1;
+        Console.WriteLine("Temperatur erhöht");
+    
+    }
+    
+    public void Senken()
+    {
+        GradCelsius -= 1;
+        Console.WriteLine("Temperatur gesenkt");
+    }
+
+}
