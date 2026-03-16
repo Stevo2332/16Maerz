7948e972eea24bb2c690b186c8332829744acf56
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 19:30:24 +0100

    Lamptask

---
+++ UebungLampe/UebungLampe.20260316/Lampe.cs
@@ -0,0 +1,34 @@
+﻿namespace UebungLampe._20260316;
+
+public class Lampe
+{
+public string Farbe { get; set; } = string.Empty;   
+    //Ein bool ist dafür zuständig, ob die Lampe an oder aus ist. Bool benutz True oder False , um den Zustand zu beschreiben. 
+    public bool IstEingeschaltet { get; set; }
+
+    // Hier wird zum Beispiel der Bool genutzt um zu sehen ob die Lampe an oder aus ist. Je nachdem wird eine Nachricht ausgegeben.
+    public void Einschalten()
+    {
+        IstEingeschaltet = true;
+    }
+
+    public void Ausschalten()
+    {
+        IstEingeschaltet = false;
+    }
+    // Hier wird zum Beispiel der Bool genutzt um zu sehen ob die Lampe an oder aus ist. Je nachdem wird eine Nachricht ausgegeben.
+
+    public void StatusAnzeigen()
+    {
+        if (IstEingeschaltet)
+        {
+            Console.WriteLine($"Die {Farbe} Lampe ist eingeschaltet.");
+        }
+        else
+        {
+            Console.WriteLine($"Die {Farbe} Lampe ist ausgeschaltet.");
+        }
+    }
+
+
+}
---
+++ UebungLampe/UebungLampe.20260316/Program.cs
@@ -0,0 +1,16 @@
+﻿using UebungLampe._20260316;
+
+
+// In diesem Beispiel erstellen wir eine Klasse namens "Lampe", die Eigenschaften wie "Farbe" und "IstEingeschaltet" hat.   
+Lampe lampe1 = new Lampe(); 
+
+lampe1.Farbe = "Rote";  
+lampe1.Einschalten();
+lampe1.StatusAnzeigen();
+
+
+// Hier erstellen wir eine zweite Lampe, die grün ist und ausgeschaltet bleibt. Wir rufen die Methode "StatusAnzeigen" auf, um den aktuellen Zustand der Lampe anzuzeigen.
+Lampe lampe2 = new Lampe();
+lampe2.Farbe = "Grüne";
+lampe2.Ausschalten();
+lampe2.StatusAnzeigen();
