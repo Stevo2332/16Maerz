3b19ec60aab611e55464d267b765206e41316cb7
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 19:09:01 +0100

    code for 2nd task

---
+++ UebungAuto2/UebungAuto2.20260316/Auto.cs
@@ -0,0 +1,17 @@
+﻿namespace UebungAuto2._20260316;
+
+public class Auto
+{
+    public string Marke { get; set; }
+    public string Modell { get; set; }
+    public int Baujahr { get; set; }
+
+
+
+    public void InfoAusgeben()
+    {
+        Console.WriteLine($"Marke: {Marke}");
+        Console.WriteLine($"Modell: {Modell}");
+        Console.WriteLine($"Baujahr: {Baujahr}");
+    }   
+}
---
+++ UebungAuto2/UebungAuto2.20260316/Program.cs
@@ -0,0 +1,19 @@
+﻿using UebungAuto2._20260316;
+
+Auto auto1 = new Auto
+{
+    Marke = "Audi",
+    Modell = "A4",
+    Baujahr = 2020 
+};
+
+Auto auto2 = new Auto
+{
+    Marke = "BMW",
+    Modell = "3er",
+    Baujahr = 2019 
+};
+
+
+auto1.InfoAusgeben();
+auto2.InfoAusgeben();
