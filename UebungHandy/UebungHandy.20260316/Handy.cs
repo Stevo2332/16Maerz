bd10625e64bd2d5660a46e266a09e3b460ae2400
Stefan Simic <ssimic@schueler.hakzell.at>
2026-03-16 20:13:12 +0100

    Handytask

---
+++ UebungHandy/UebungHandy.20260316/Handy.cs
@@ -0,0 +1,25 @@
+﻿namespace UebungHandy._20260316;
+
+public class Handy
+{
+public string Marke { get; set; }   
+public string Modell { get; set; }  
+
+public string AkkuProzent { get; set; } 
+
+
+
+    public void Anzeigen()
+    {
+        Console.WriteLine($"Marke: {Marke}");
+        Console.WriteLine($"Modell: {Modell}");
+        Console.WriteLine($"Akku: {AkkuProzent}%");
+    }   
+
+    public void Aufladen()
+    {
+        AkkuProzent = "100";
+        Console.WriteLine("Handy aufgeladen!");
+    }
+
+}
---
+++ UebungHandy/UebungHandy.20260316/Program.cs
@@ -0,0 +1,10 @@
+﻿using UebungHandy._20260316;
+
+Handy handy1 = new Handy(); 
+handy1.Marke = "Samsung Galaxy";
+handy1.Modell = "S23 Ultra";    
+handy1.AkkuProzent = "50";
+
+handy1.Anzeigen();
+handy1.Aufladen();
+handy1.Anzeigen();
