b3a06657e2dd80daaffcf81a005d8ea9c3a7c8b5
Simic Stefan <simicstefan@hakzell.schueler.at>
2026-03-16 15:10:02 +0100

    adding car1

---
+++ UebungAuto/UebungAuto.20260316/Auto1.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace UebungAuto._20260316;
+
+public class Auto1
+{
+   
+    public string Marke { get; set; } = string.Empty;     
+    public string Modell { get; set; } = string.Empty;  
+    public int Baujahr { get; set; }
+
+    public void InfoAusgeben()
+    {
+        Console.WriteLine($"Marke: {Marke}");
+        Console.WriteLine($"Modell: {Modell}");
+        Console.WriteLine($"Baujahr: {Baujahr}");
+    }
+
+}
+
---
+++ UebungAuto/UebungAuto.20260316/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using UebungAuto._20260316;
+
+Auto1 auto1 = new Auto1();
+
+
+auto1.Marke = "BMW";
+auto1.Modell = "X3";
+auto1.Baujahr = 2022;
+
+auto1.InfoAusgeben();
